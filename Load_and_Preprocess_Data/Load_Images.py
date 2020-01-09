35468e8f1c0c77beb9f7f43e99d4b4d51c8a8c6e
박주환 <jng94@sports2i.com>
2020-01-09 18:22:09 +0900

    setting Load_Images.py

---
+++ Load_and_Preprocess_Data/Load_Images.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+import IPython.display as display
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+print(tf.__version__)
